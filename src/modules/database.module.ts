ebb2b5479c071c4c40993df67a46d32aba03f564
Erwin Errol Calanuga <hortsatta@gmail.com>
2024-06-06 17:34:12 +0800

    Update database settings

--- src/modules/database.module.ts
+++ src/modules/database.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
-import cfetch from 'cross-fetch';
 
 import { User } from './user/entities/user.entity';
 import { UserProfile } from './user/entities/user-profile.entity';
@@ -17,12 +16,8 @@ import { TodaAssociation } from './franchise/entities/toda-association.entity';
         let ssl: object | boolean = false;
         // Fetch pem file for aws rds db in prod
         if (process.env.NODE_ENV === 'production') {
-          const pemUrl = configService.get<string>('DATABASE_PEM_URL');
-          const response = await cfetch(pemUrl);
-          const ca = await response.text();
           ssl = {
             rejectUnauthorized: false,
-            ca,
           };
         }
 
